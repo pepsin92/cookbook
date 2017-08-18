892aede4226abf9952c37e3f09deed1959e00485
sandyna <melaniemyxa@gmail.com>
2017-08-18 17:41:24 +0200

    incorporated add_recipe into the page

--- index.py
+++ index.py
@@ -1,3 +1,3 @@
 from flask import Flask
 
-app = Flask(__name__, static_folder="./static/dist", template_folder="./static")
+app = Flask(__name__, static_folder="./static", template_folder="./static")
